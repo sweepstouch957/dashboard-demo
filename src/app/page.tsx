c56d87e0bfd94964f0d2f80cd547340e6a4c38d6
sweepstouch957 <sweepstouchcloud@gmail.com>
2025-04-26 09:05:41 -0600

    add text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import CampaignDashboard from "@/components/Dashboard";
 export default function Home() {
   return (
     <div className="">
-      <h1 className="text-2xl font-bold">Welcome to the Campaign Dashboard</h1>
 
       <CampaignDashboard />
     </div>
